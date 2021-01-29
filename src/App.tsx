6998ca0af6819b768839fbc6d52d26282e7a425d
Leandro Nunes <nunes.pessoal@gmail.com>
2021-01-28 21:10:40 -0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios'
 import './App.css';
+import { Endereco } from './types/endereco';
 
 function App() {
+
+  const [cep, setCep] = useState<String>("")
+
+  const[endereco, setEndereco] = useState<Endereco>()
+
+  const getCep = () => {
+    axios.get(`https://viacep.com.br/ws/${cep}/json/`)
+    .then(resposta => setEndereco(resposta.data))
+  }
+
+  console.log('endereco', endereco)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" onChange={(event) => setCep(event.target.value)}/>
+      <button onClick={getCep}>Ver CEP</button>
+      <h1>Endereço</h1>
+      {
+        <p>O CEP {endereco?.cep} corresponde à rua {endereco?.logradouro}, no bairro {endereco?.bairro}, na cidade de {endereco?.localidade}, {endereco?.uf}</p>
+      }
     </div>
   );
 }
---
+++ src/types/endereco.ts
@@ -0,0 +1,9 @@
+export interface Endereco {
+    bairro: string,
+    cep: string,
+    complemento: string,
+    ddd: number,
+    localidade: string,
+    logradouro: string,
+    uf: string
+}
